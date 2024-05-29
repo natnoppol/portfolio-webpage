9a7b54852cecff7d9d05e58a0da0ba37268be5cc
natnoppol <126169607+natnoppol@users.noreply.github.com>
2024-05-30 00:14:17 +0200

    update css

--- js/libs.mjs
+++ js/libs.mjs
@@ -1,4 +1,19 @@
+let i = 0;
+const text = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, magnam?!';
+let speed = 50;
+
 function navResponsive(){
     const navBar = document.querySelector(".navigation");
       navBar.classList.toggle("active")
   }
+
+function typingEffect() {
+  if (i < text.length) {
+    document.getElementById("typingEffect").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typingEffect, speed);
+  }
+}
+window.addEventListener('load',() =>{
+  typingEffect()
+})
