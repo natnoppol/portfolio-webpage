ec382b677b6847c2754e05ea8ccc19a9a2ae69f3
natnoppol <126169607+natnoppol@users.noreply.github.com>
2024-05-31 02:17:27 +0200

    update css

--- js/libs.mjs
+++ js/libs.mjs
@@ -2,7 +2,6 @@ let i = 0;
 const text = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, magnam?!';
 let speed = 50;
 const textcon = document.getElementById('typingEffect');
-textcon.textContent = text;
 textcon.classList.add('p-active')
 
 function navResponsive(){
