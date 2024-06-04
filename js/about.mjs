7740eba5ef2a899fe3b0fd5dc6d657473222a249
natnoppol <126169607+natnoppol@users.noreply.github.com>
2024-06-04 18:37:05 +0200

    update about page

--- js/about.mjs
+++ js/about.mjs
@@ -1,11 +1,13 @@
 const tabLinks = document.getElementsByClassName('tab-links');
 const tabContents = document.getElementsByClassName('tab-content');
 
-function openTab(tabName){
+function openTab(tabname){
     for(let tablink of tabLinks){
         tablink.classList.remove('active-link')
     }
     for(let tabcontent of tabContents){
         tabcontent.classList.remove('active-tab')
     }
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(tabname).classList.add('active-tab')
 }
